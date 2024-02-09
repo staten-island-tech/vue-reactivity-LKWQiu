bdf0691fadc7eb8e1d2467f9b133fb1c50a6ce86
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-02-09 09:22:31 -0500

    jlkjkl

---
+++ vue/src/views/InventoryView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Inventory">
+    <p v-if="amount > 0">
+      You have {{ amount }} stars
+    </p>
+
+    <p v-else>
+      U have nothing in your inventory.
+      <br>
+      Use the gatcha.
+    </p>
+
+    <button v-on:click="RollGatch()">
+      Roll
+    </button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .Inventory {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default{
+  data() {
+    return {
+      amount: 0,
+    }
+  },
+  methods: {
+    RollGatch(){
+      this.amount++;
+      alert('U rolled something!!!')
+    }
+  },
+}
+
+</script>
